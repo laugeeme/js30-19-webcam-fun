bf4ffeaf114da8c9aa69e25082c89fdaf18ca4aa
laugeeme <registro.laura@gmail.com>
2020-11-04 19:28:32 +0100

    active media in navigator

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
+'use strict';
+
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      console.log(localMediaStream);
+    });
+}
+
+
+getVideo();

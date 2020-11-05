58f3adaa98665c33c7f1ced6ed44c5ca1cd41314
laugeeme <registro.laura@gmail.com>
2020-11-05 13:08:07 +0100

    fix red filter

--- scripts.js
+++ scripts.js
@@ -26,6 +26,12 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    //take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    //mess with them
+    pixels = redEffect(pixels);
+    //put them back
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -40,8 +46,16 @@ function takePhoto() {
   link.href = data;
   link.setAttribute('download', 'capture');
   link.innerHTML = `<img src ="${data}" alt="Capture" />`;
-  
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; //RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //BLUE
+  }
+  return pixels;
 }
 
 getVideo();

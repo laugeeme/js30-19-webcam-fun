598211ccf2aa7380fe6a567cb3d505181ec66d05
laugeeme <registro.laura@gmail.com>
2020-11-05 12:37:52 +0100

    paint the frame

--- scripts.js
+++ scripts.js
@@ -23,7 +23,11 @@ function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
   canvas.width = width;
-  canvas.height= height;
+  canvas.height = height;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
 }
 
 getVideo();

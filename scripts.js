cd5f2bac57ece62511c3a2f7bcb9535a0ec5ef9e
laugeeme <registro.laura@gmail.com>
2020-11-05 12:58:30 +0100

    change download link in a picture link

--- scripts.js
+++ scripts.js
@@ -38,8 +38,9 @@ function takePhoto() {
   const data = canvas.toDataURL('image/jpeg');
   const link = document.createElement('a');
   link.href = data;
-  link.setAttribute('download', 'handsome');
-  link.textContent = 'Download Image';
+  link.setAttribute('download', 'capture');
+  link.innerHTML = `<img src ="${data}" alt="Capture" />`;
+  
   strip.insertBefore(link, strip.firstChild);
 }
 

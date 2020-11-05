0cc79e7e6ae7094b978c0416ed8b441157d48b8c
laugeeme <registro.laura@gmail.com>
2020-11-05 13:31:17 +0100

    add function to play with buttons

--- scripts.js
+++ scripts.js
@@ -30,7 +30,9 @@ function paintToCanvas() {
     let pixels = ctx.getImageData(0, 0, width, height);
     //mess with them
     /*     pixels = redEffect(pixels); */
-    pixels = rgbSplit(pixels);
+    /*     pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.8; */
+    pixels = greenEffect(pixels);
     //put them back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
@@ -62,9 +64,36 @@ function redEffect(pixels) {
 function rgbSplit(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
     pixels.data[i - 150] = pixels.data[i + 0]; //RED
-    pixels.data[i + 100] = pixels.data[i + 1]; //GREEN
+    pixels.data[i + 500] = pixels.data[i + 1]; //GREEN
     pixels.data[i - 150] = pixels.data[i + 2]; //BLUE
   }
+  return pixels;
+}
+
+function greenEffect(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (let i = 0; i < pixels.data.length; i = i + 4) {
+    let red = pixels.data[i + 0];
+    let green = pixels.data[i + 1];
+    let blue = pixels.data[i + 2];
+    let alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      pixels.data[i + 3] = 0;
+    }
+  }
+
   return pixels;
 }
 
